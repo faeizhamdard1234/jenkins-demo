f1b0185c66b59f31f1c7fe0df4583bc09d73b825
Faeiz <faeizhamdrad48@gmail.com>
2025-05-09 13:54:39 +0430

    Simple project 2 - interactive script

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Running Python script with Jenkins"
+echo "Faeiz" | python3 script.py
---
+++ script.py
@@ -0,0 +1,4 @@
+import os
+
+name = input("Enter your name: ")
+print(f"Hello, {name}! Welcome to Jenkins automation.")
